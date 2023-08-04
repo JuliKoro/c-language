6e07ea5a654901d4f55acf9098b18479fbe9cccd
Julian <julian.koroluk@outlook.com>
2023-08-03 22:41:06 -0300

    Primer commit Hola Mundo!

---
+++ ANSIC-Exercises/Chapter1/exercise1-1.c
@@ -0,0 +1,16 @@
+/*
+Exercise 1-1. Run the ``hello, world'' program on your system. Experiment with leaving
+out parts of the program, to see what error messages you get.
+
+Exercise 1-2. Experiment to find out what happens when prints's argument string contains
+\c, where c is some character not listed above.
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello World!\n");
+
+    return 0;
+}
